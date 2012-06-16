abc2cc64ed1f5dd613e102044cda0d52fbda74a5
DAHMEN Manuel <manuel.dahmen@gmail.com>
2012-06-16 09:52:44 +0200

    version 1

---
+++ src/emptycanvas/viewer/EmptycanvasViewer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emptycanvas.viewer;
+
+import java.io.File;
+
+/**
+ *
+ * @author Manuel DAHMEN
+ */
+public class EmptycanvasViewer {
+    public EmptycanvasViewer(File dir, String type)
+    {
+        
+    }
+    public EmptycanvasViewer(File dir, boolean encode)
+    {
+        
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
